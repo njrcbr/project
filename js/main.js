f1bc6ad47ef92422d6764c26d35cb19266726fa1
njrcbr <njrcbr@gmail.com>
2021-04-27 17:56:12 +0300

    slider add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/prev.svg" alt="next arrow"></button>',
+        prewArrow: '<button class="slick-arrow slick-prev"><img src="../images/prev.svg" alt="prev arrow"></button>        '
+    });
 });
+
